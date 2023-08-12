e26fa4d56adc3be9d00d662e1cba926305626b54
Andrey Fedorov <fvandre@outlook.com>
2023-08-12 15:39:18 +0300

    Первое задание сделано

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,24 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let discriminant = b ** 2 - 4 * a * c;
+	let result = [];
+
+	if (discriminant === 0) {
+		result.push(-b / (2 * a));
+	} else if (discriminant > 0) {
+		result.push((-b + Math.sqrt(discriminant)) / (2 * a));
+		result.push((-b - Math.sqrt(discriminant)) / (2 * a));
+	}
+	return result;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+	let monthlyPersent = percent / 100 / 12;
+	let sumCredit = amount - contribution;
+
+	let monthlyPayment = sumCredit * (monthlyPersent + (monthlyPersent / (((1 + monthlyPersent) ** countMonths) - 1)));
+
+	return Number((monthlyPayment * countMonths).toFixed(2));
 }
