72fb51621b9f86e46bf8ed1ba9053bfd0be99a70
Andrey Fedorov <fvandre@outlook.com>
2023-09-06 01:02:40 +0300

    Третье задание сделано

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
+"use strict"
+
 function compareArrays(arr1, arr2) {
-  
+	return ((arr1.length === arr2.length) && arr1.every((element, index) => element === arr2[index]));
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	if (gender === "мужской" || gender === "женский" && users.length > 0) {
+		return users.filter(usersGender => usersGender.gender === gender)
+			.map(usersAge => usersAge.age)
+			.reduce((acc, age, index, arr) => {
+				acc += age;
+				if (index === arr.length - 1) {
+					return acc / arr.length;
+				}
+				return acc;
+			}, 0);
+	}
+	return 0;
 }
